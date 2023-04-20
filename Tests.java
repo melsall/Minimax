398dc491a2612127f5a05a4964fa5860793c8fa9
melsall <93349505+melsall@users.noreply.github.com>
2023-04-20 14:12:34 -0400

    made Tests.java file and wrote a test case

---
+++ Tests.java
@@ -0,0 +1,6 @@
+public class Tests {
+    public static void main(String [] args){
+        System.out.println("Testing my NimRunner class.");
+        System.out.println(NimRunner.minimax(1,true) == -1);
+    }
+}
