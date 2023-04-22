31ea1ec8b5cfe2470b16501bce7cb623495e6bf3
melsall <93349505+melsall@users.noreply.github.com>
2023-04-22 17:57:49 -0400

    was getting varying results from minimax function. Started fresh with the function taking inspiratation/some code from old version

--- NimRunner.java
+++ NimRunner.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io;
+import java.io.*;
 public class NimRunner {
     // instance variables of class
     // method to run the game
@@ -11,9 +11,9 @@ public class NimRunner {
 
     public static boolean runGame(){
         boolean isXTurn = true;
-        int state = Math.floor(Math.random() *(15 - 1 + 1) + 1);
+        int state = (int) Math.floor(Math.random() *(15 - 1 + 1) + 1); // generatinga  random state
         while(state >= 1){
-            if (isXturn){
+            if (isXTurn){
                 // updating state to subtract the correct number of objects
                 state = getXMove(state);
 
@@ -29,7 +29,7 @@ public class NimRunner {
 
     // figure out where X will move
     public static int getXMove(int state){
-        int chosenInt;
+        int chosenInt = 0;
         while(chosenInt > state){
             chosenInt = (int) Math.floor(Math.random() *(3 - 1 + 1) + 1);
         }
@@ -38,13 +38,42 @@ public class NimRunner {
 
     // figure out where Y will move
     public static int getYMove(int state){
-        int chosenInt;
+        int chosenInt = 0;
         while(chosenInt > state){
             chosenInt = (int) Math.floor(Math.random() *(3 - 1 + 1) + 1);
         }
         return state - chosenInt;
     }
 
+    // NEW MINIMAX FUNCTION
+    public static int minimax(int state, boolean myTurn){
+        //base case (state = 0)
+        if (state == 0){
+            if (myTurn){
+                return 1; // if it is my turn and the state is 0, I won
+            }
+            else{
+                return -1; // if it is not my turn and the state is 0, I lost
+            }
+        }
+        else{
+            ArrayList <Integer> listOfScores = new ArrayList <> ();
+            listOfScores.add(minimax(state - 1, !myTurn));
+            if(state - 2 >= 0){ // check to make sure subtracting 2 pieces will not result in a negative number of pieces (because that's not possible)
+                listOfScores.add(minimax(state - 2, !myTurn));
+            }
+            if(state - 3 >= 0){ // check to make sure subtracting 3 pieces will not result in a negative number of pieces (because that's not possible)
+                listOfScores.add(minimax(state - 3, !myTurn));
+            }
+            if(myTurn){
+                return Collections.max(listOfScores); // if it is your turn, you want the max of the list to be returned
+            }
+            else{ // if it's not your turn, you want the min of the list to be returned
+                return Collections.min(listOfScores);
+            }
+        }
+    }
+    /* 
     public static int minimax(int state, boolean myTurn){
         // base case: state = 0
         if (state == 0){
@@ -64,13 +93,14 @@ public class NimRunner {
                 }
             }
             if (myTurn){
-                return listOfScores.max();
+                return Collections.max(listOfScores);
             }
             else{
-                return listOfScores.min();
+                return Collections.min(listOfScores);
             }
         }
     }
+    */ 
 // if minimax - piecesTaken > 0 , return high 
     
     // call minimax three times in your recursive step, if you hit -1, return the minimum 
