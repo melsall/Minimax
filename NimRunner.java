bfa758e797e4b0bc599b3ac96295e55e6fb72962
melsall <93349505+melsall@users.noreply.github.com>
2023-04-25 22:07:58 -0400

    I think I wrote the minimax method in a valid way but not sure if what I did at the end is right sicne I just randomly changed things until it got the test cases to work

--- NimRunner.java
+++ NimRunner.java
@@ -4,10 +4,12 @@ public class NimRunner {
 
     public static void main (String [] args){
         System.out.println(makeRandomStates());
-
-
+        ArrayList <Integer> rands = new ArrayList <> ();
+        rands.add(1);
+        rands.add(1);
+        System.out.println(minimax(rands, false));
     }
-    // // function that actually runs game/ simulates real game play
+    // // // function that actually runs game/ simulates real game play
     // public static boolean runGame(){
     //     boolean isXTurn = false;
     //     // make state a 2D array?
@@ -45,7 +47,7 @@ public class NimRunner {
     public static ArrayList <Integer> makeRandomStates(){
         ArrayList <Integer> states = new ArrayList <>();
         for(int i = 0; i < (int)(Math.random()*15) + 1; i++){
-            states.add((int) (Math.random() * 15 ) + 1); // 
+            states.add((int) (Math.random() * 15 ) + 1); // adding pile of random size
         }
         return states;
     }
@@ -67,64 +69,69 @@ public class NimRunner {
     //     return state - bestMove(state, true);
     // }
 
-//     // Minimax Function
-//     public static int minimax(ArrayList <Integer> states, boolean myTurn){
-//         //base case (there are no objects in any of the piles)
-//         if (isBaseCase(states)){
-//             if (myTurn){
-//                 return 1; // if it is my turn and the state is 0, I won
-//             }
-//             else{
-//                 return -1; // if it is not my turn and there are no objects, I lost
-//             }
-//         }
-//         else{
-//             ArrayList <ArrayList <Integer>> moves = new ArrayList <> (); // make a moves array to hold the list of all the possible moves
-//             for (int p = 0; p < states.size(); p++){ // iterate through each pile
-//                 int [][] currentMove = new int [states.size];
-//                 for (int c = 1; c < states.get(p); c++){ // iterate through each object in the piles
-//                     currentMove [r] = c; // making array of the possible moves, taking c objects each time
-//                 }
-//                 moves.add(Arrays.asList(currentMove)); // adding the possible moves for a given pile to the moves list
-//             }
-//             // evaluating how "good" all of the moves are
-//             ArrayList <ArrayList <Integer>> states = new ArrayList <> ();
-//             for(int r = 0; r < states.size(); r++){
-//                 for (int c = 0; c < states.size(); c++){
-                
-//                 }
-
-//             }
-//         }
-//         return 10000; 
-//     }
-//     // helper function to see if it is the base case for minimax, returns true if it is the base case
-//     public static boolean isBaseCase(ArrayList <Integer> states){
-//         for(int i = 0; i < states; i++){
-//             if(states.get(i) != 0){
-//                 return false; // if one of the piles have more than 0 objects, it is not the base case
-//             }
-//         }
-//         return true; // if you have iterated through the whole for loop and all of the piles have 0, it is the base case
-//     }
-//     // // Best Move Function: 
-//     // public static int bestMove(int state, boolean myTurn){
-//     //     for(int i = 1; i <= 3; i++){ // loop through all of the possible moves (taking 1, 2, or 3)
-//     //         if (state - i >= 0 && myTurn){
-//     //             if (minimax(state - i, !myTurn) == 1){ // have to make the boolean condition for minimax !myTurn rather than myTurn because once I take these pieces, it will become the other player's turn
-//     //                 return i; // if taking i pieces results in 1 being returned, immediately return it becasue that means you will automatically win
-//     //             }
-//     //         }
-//     //         // finding the best move for the other player
-//     //         else if(state - i >= 0 && !myTurn){
-//     //             if (minimax(state - i, myTurn) == 1){
-//     //                 System.out.println("when Y takes they take: " + i + "pieces");
-//     //                 return i; // if taking i pieces results in -1 being returned, immediatley return i bceasue you win
-//     //             }
-//     //         }
-//     //     }
-//     //     return 1;
-//     // }
+    // Minimax Function
+    public static int minimax(ArrayList <Integer> states, boolean myTurn){
+        //base case (there are no objects in any of the piles)
+        if (isBaseCase(states)){
+            if (myTurn){
+                return 1; // if it is my turn and the state is 0, I won
+            }
+            else{
+                return -1; // if it is not my turn and there are no objects, I lost
+            }
+        }
+        else{
+            // if it's not the base case, have to iterate through each pile
+            for (int p = 0; p < states.size(); p++){ // iterate through each pile
+                for (int c = 0; c < states.get(p); c++){ // iterate through each object in the piles
+                    ArrayList <Integer> currentPiles = new ArrayList <Integer> (states); // holder to test results of different states
+                    currentPiles.set(p, c); // setting the current pile to have c objects to see if it would be favorable for minimax
+                    // recursive step
+                    if (myTurn){
+                        if (minimax(currentPiles, !myTurn) == 1){
+                            return 1;  
+                        }
+                    }
+                    else{
+                        if(minimax(currentPiles, !myTurn) == -1){
+                            return -1;
+                        }
+                    }
+                }
+            }
+        }
+        if (myTurn){
+            return -1;
+        }
+        return 1;
+    }
+    // helper function to see if it is the base case for minimax, returns true if it is the base case
+    public static boolean isBaseCase(ArrayList <Integer> states){
+        for(int i = 0; i < states.size(); i++){
+            if(states.get(i) != 0){
+                return false; // if one of the piles have more than 0 objects, it is not the base case
+            }
+        }
+        return true; // if you have iterated through the whole for loop and all of the piles have 0, it is the base case
+    }
+    // // // Best Move Function: 
+    // public static int bestMove(int state, boolean myTurn){
+    //     for(int i = 1; i <= 3; i++){ // loop through all of the possible moves (taking 1, 2, or 3)
+    //         if (state - i >= 0 && myTurn){
+    //             if (minimax(state - i, !myTurn) == 1){ // have to make the boolean condition for minimax !myTurn rather than myTurn because once I take these pieces, it will become the other player's turn
+    //                 return i; // if taking i pieces results in 1 being returned, immediately return it becasue that means you will automatically win
+    //             }
+    //         }
+    //         // finding the best move for the other player
+    //         else if(state - i >= 0 && !myTurn){
+    //             if (minimax(state - i, myTurn) == 1){
+    //                 System.out.println("when Y takes they take: " + i + "pieces");
+    //                 return i; // if taking i pieces results in -1 being returned, immediatley return i bceasue you win
+    //             }
+    //         }
+    //     }
+    //     return 1;
+    // }
 
 
 // /*
