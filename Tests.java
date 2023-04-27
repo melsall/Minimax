1758597685a13e4bb851ad46c4dbfcde803113c7
melsall <93349505+melsall@users.noreply.github.com>
2023-04-26 23:41:02 -0400

    made empty Reflection file

--- Tests.java
+++ Tests.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class Tests {
     public static void main(String [] args){
         System.out.println("Testing my NimRunner class.");
-        System.out.println(NimRunner.minimax(1,true) == -1);
+
+        // ArrayList I know that the bot should be able to win: making sure minimax algorithm actually works
+        ArrayList <Integer> winnableTest = new ArrayList <> ();
+        winnableTest.add(1);
+        winnableTest.add(2);
+        winnableTest.add(3);
+        System.out.println(NimRunner.minimax(winnableTest, true) == 1); // should return true beause minimax should be able to win if this is the starting stae
+        //System.out.println(NimRunner.minimax(0, true));
     }
 }
